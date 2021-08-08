2c71d579a1a02acb6c4785e0fded2d2a212b7b2b
Daniel Di Mella <bigbigbyned@gmail.com>
2021-08-08 15:03:46 -0400

    Minor QoL changes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -18,10 +18,8 @@ class UsersController {
       const { id } = req.params;
       await pool.query('SELECT * FROM usuario WHERE cedula = $1;', [id], function (e, res) {
         if (e) throw e;
-        response.status(200).json(res.rows)
-        return res.rows;
+        response.status(200).json(res.rows[0])
       });
-      response.status(200).json(data)
     } catch (e) {
       response.status(400).json(e)
     }
@@ -40,14 +38,12 @@ class UsersController {
         clave,
         telefono,
       } = req.body;
-      const data = await pool.query('INSERT INTO usuario (cedula, id_carrera, is_admin, rol, nombre, apellido, email, clave, telefono) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);',
+      await pool.query('INSERT INTO usuario (cedula, id_carrera, is_admin, rol, nombre, apellido, email, clave, telefono) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);',
         [cedula, idCarrera, isAdmin, rol, nombre, apellido, email, clave, telefono],
         function (e, res) {
           if (e) throw e;
-          console.log('Se insertaron ' + res.affectedRows + ' campos');
-          return res;
+          response.status(200).json('Usuario creado')
         });
-      response.status(200).json(data)
     } catch (e) {
       response.status(400).json(e)
     }
@@ -67,14 +63,12 @@ class UsersController {
         clave,
         telefono,
       } = req.body;
-      const data = await pool.query('UPDATE usuario SET id_carrera=$1, is_admin=$2, rol=$3, nombre=$4, apellido=$5, email=$6, clave=$7, telefono=$8 WHERE cedula=$9',
+      await pool.query('UPDATE usuario SET id_carrera=$1, is_admin=$2, rol=$3, nombre=$4, apellido=$5, email=$6, clave=$7, telefono=$8 WHERE cedula=$9',
         [idCarrera, isAdmin, rol, nombre, apellido, email, clave, telefono, id],
         function (e, res) {
           if (e) throw e;
-          console.log('Usuario actualizado ');
-          return res;
+          response.status(200).json('Usuario actualizado')
         });
-      response.status(200).json(data)
     } catch (e) {
       response.status(400).json(e)
     }
@@ -83,12 +77,10 @@ class UsersController {
   async deleteUser(req, response) {
     try {
       const { id } = req.params;
-      const data = await pool.query('DELETE FROM usuario WHERE cedula = $1;', [id], function (e, res) {
+      await pool.query('DELETE FROM usuario WHERE cedula = $1;', [id], function (e, res) {
         if (e) throw e;
-        console.log(res);
-        return res;
+        response.status(200).json('Usuario borrado')
       });
-      response.status(200).json(data)
     } catch (e) {
       response.status(400).json(e)
     }
